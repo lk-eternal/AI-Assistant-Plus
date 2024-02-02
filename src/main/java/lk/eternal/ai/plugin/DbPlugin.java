e151451eed476ba7bdfae44f68ea307d39d9c87e
lian <an.li@wukongsch.com>
2024-02-02 17:28:55 +0800

    feat: 修改README

--- src/main/java/lk/eternal/ai/plugin/DbPlugin.java
+++ src/main/java/lk/eternal/ai/plugin/DbPlugin.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Component
+//@Component
 public class DbPlugin implements Plugin {
 
     public DbPlugin() {
--- src/main/java/lk/eternal/ai/plugin/SshPlugin.java
+++ src/main/java/lk/eternal/ai/plugin/SshPlugin.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Component
+//@Component
 public class SshPlugin implements Plugin {
 
     @Override
